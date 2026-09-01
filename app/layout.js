00ad9dc5b321959fd4d116c113a5e5393b22da4b
BlackLabelBranding <lance@garzaunlimited.com>
2026-08-31 23:04:55 -0500

    Version iPhone PWA icon to bypass cache

--- app/layout.js
+++ app/layout.js
@@ -12,7 +12,7 @@ export const metadata = {
       { url: '/icon-192.png', sizes: '192x192', type: 'image/png' },
       { url: '/icon-512.png', sizes: '512x512', type: 'image/png' }
     ],
-    apple: [{ url: '/apple-icon.png', sizes: '180x180', type: 'image/png' }]
+    apple: [{ url: '/sherwin-safety-icon-180.png', sizes: '180x180', type: 'image/png' }]
   },
   appleWebApp: {
     capable: true,
